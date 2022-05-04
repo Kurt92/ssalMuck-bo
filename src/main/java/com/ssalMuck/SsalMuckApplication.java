8dd579275b128e21b00e27cbd70dd49e1ade2715
Kurt92 <chcch1234@gmail.com>
2022-05-04 18:50:11 +0900

    1st

---
+++ src/main/java/com/ssalMuck/SsalMuckApplication.java
@@ -0,0 +1,13 @@
+package com.ssalMuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsalMuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsalMuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssalMuck/SsalMuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssalMuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsalMuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
