c8e0de9ee521c1776cb92ed11242e66090923b40
Kurt92 <chcch1234@gmail.com>
2022-05-20 18:16:47 +0900

    Junit test코드 작성

---
+++ src/main/java/com/ssalMuck/Repository/OrderRepository.java
@@ -0,0 +1,25 @@
+package com.ssalMuck.Repository;
+
+import com.ssalMuck.Entity.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    //검색
+    //public List<Order> findAll(OrderSearch orderSearch){}
+}
