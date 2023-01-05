9803b6b0b8ffd49fff2e55647fbf53bbe34b8daf
Kurt92 <chcch1234@gmail.com>
2023-01-05 18:04:58 +0900

    jenkins test4 => @SpringBootTest remove

--- src/test/java/com/ssalMuck/SsalMuckApplicationTests.java
+++ src/test/java/com/ssalMuck/SsalMuckApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssalMuck;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest*/
 class SsalMuckApplicationTests {
 
 	@Test
